ed3c196f1eb0ba8f73cf7ea3ab9e7a978c85ebc2
Aviram Benshimol <avirambe@avirambe-mbp.home>
2021-01-20 23:33:43 +0200

    updated css to scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
